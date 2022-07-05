cf841b21166d58a228a421b9bffc2ad67c0da477
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-05 13:41:59 +0530

    Added If , else if and else statement

---
+++ app/loopsandconditionalstatements/src/main/java/com/example/loopsandconditionalstatements/if_and_when_expressions.kt
@@ -0,0 +1,12 @@
+package com.example.loopsandconditionalstatements
+
+fun main(){
+    val amount = 500
+    if(amount == 450){
+        println("Less amount")
+    }else if(amount == 500){
+        println("Enough")
+    }else{
+        println("work hard")
+    }
+}
