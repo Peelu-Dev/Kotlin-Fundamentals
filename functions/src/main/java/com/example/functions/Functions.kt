62b8d4feaf38156788ba06d0cf9f60e9ba129778
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-05 14:12:05 +0530

    Added function with Int Parameters

--- functions/src/main/java/com/example/functions/Functions.kt
+++ functions/src/main/java/com/example/functions/Functions.kt
@@ -1,10 +1,18 @@
 package com.example.functions
 
 fun main(){
- sayHello()
+ sayHelloWithParameters(1,200,6)
 }
-fun sayHello(){
-    for(i in 1..10){
-        println("$i")
+//fun sayHello(){
+//    for(i in 1..10){
+//        println("$i")
+//    }
+//}
+
+fun sayHelloWithParameters(first:Int,second:Int,multipleOf:Int){
+    for(i in first..second){
+        if(i % multipleOf == 0){
+            println("$i is a multiple of $multipleOf")
+        }
     }
 }
