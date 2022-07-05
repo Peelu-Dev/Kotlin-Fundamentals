9245e45f8a2f5fb9cd349d492064bcf6e3a8ab58
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-05 13:46:59 +0530

    Added when expression uses

--- app/loopsandconditionalstatements/src/main/java/com/example/loopsandconditionalstatements/if_and_when_expressions.kt
+++ app/loopsandconditionalstatements/src/main/java/com/example/loopsandconditionalstatements/if_and_when_expressions.kt
@@ -1,12 +1,20 @@
 package com.example.loopsandconditionalstatements
 
 fun main(){
-    val amount = 500
-    if(amount == 450){
-        println("Less amount")
-    }else if(amount == 500){
-        println("Enough")
-    }else{
-        println("work hard")
+
+//    if(amount == 450){
+//        println("Less amount")
+//    }else if(amount == 500){
+//        println("Enough")
+//    }else{
+//        println("work hard")
+//    }
+    when(500){
+        in 1..100 -> println("Between 1 and 100")
+        in 100..500 -> println("Between 100 and 500")
+        in 500..1000 -> println("Between 500 and 1000")
+        else -> {
+            println("Above 1000")
+        }
     }
 }
