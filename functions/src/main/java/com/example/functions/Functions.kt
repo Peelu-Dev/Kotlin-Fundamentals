3ce4e3f34f2e07db72eeeba90dde05677d51b41f
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-05 14:40:13 +0530

    Added function with return parameter type

--- functions/src/main/java/com/example/functions/Functions.kt
+++ functions/src/main/java/com/example/functions/Functions.kt
@@ -1,7 +1,9 @@
 package com.example.functions
 
 fun main(){
- sayHelloWithParameters( message = "is a multiple of", multipleOf = 7)
+// sayHelloWithParameters( message = "is a multiple of", multipleOf = 7)
+    val catAge = calculateCatAge(12)
+    println("Cat Age is $catAge")
 }
 //fun sayHello(){
 //    for(i in 1..10){
@@ -9,10 +11,14 @@ fun main(){
 //    }
 //}
 
-fun sayHelloWithParameters(first:Int=9,second:Int=90,message:String,multipleOf:Int){
-    for(i in first..second){
-        if(i % multipleOf == 0){
-            println("$i $message $multipleOf")
-        }
-    }
+//fun sayHelloWithParameters(first:Int=9,second:Int=90,message:String,multipleOf:Int){
+//    for(i in first..second){
+//        if(i % multipleOf == 0){
+//            println("$i $message $multipleOf")
+//        }
+//    }
+//}
+
+fun calculateCatAge(age:Int):Int{
+    return age * 5
 }
