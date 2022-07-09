be190dbccad7c4da666d3dea3fbf85394b2a072a
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-09 14:33:30 +0530

    Added class primary constructor

--- oops/src/main/java/com/example/oops/Class.kt
+++ oops/src/main/java/com/example/oops/Class.kt
@@ -1,16 +1,14 @@
 package com.example.oops
 
 fun main(){
-    val car = Car()
-    car.color = "Black"
-    car.model = "XUV100"
+    val car = Car(color = "Black", model = "XUV100")
+    val secondCar = Car(color = "Red", model = "Ferrari")
     println("Car Color is: ${car.color} and Car model is: ${car.model}" )
+    println("Car Color is: ${secondCar.color} and Car model is: ${secondCar.model}" )
     println(car.drive())
 }
 
-class Car{
-    var color = "Blue"
-    var model = "BMW"
+class Car(var color : String = "Blue", var model:String = "BMW"){
 
     fun drive(){
         println("Driving...")
