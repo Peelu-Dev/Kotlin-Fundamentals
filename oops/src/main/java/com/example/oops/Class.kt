d0557ab0ba6a6ba3adb45b97bc16b88225fa793e
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-09 14:53:29 +0530

    Added function with parameters inside our car class

--- oops/src/main/java/com/example/oops/Class.kt
+++ oops/src/main/java/com/example/oops/Class.kt
@@ -4,7 +4,8 @@ fun main(){
     val car = Car(color = "Black", model = "XUV100")
     val secondCar = Car(color = "Red", model = "Ferrari")
     println("Car Color is: ${car.color} and Car model is: ${car.model}" )
-    println("secondCar Color is: ${secondCar.color} and secondCar model is: ${secondCar.model}" )
+    println("secondCar Color is: ${secondCar.color} and secondCar model : ${secondCar.model}" )
+    car.speed(minSpeed = 100, maxSpeed = 300)
     println(car.drive())
 }
 
@@ -13,6 +14,10 @@ class Car(var color : String , var model:String){
 //        color = "Red"
 //        model = "XUV800"
 //    }
+
+    fun speed( minSpeed : Int, maxSpeed:Int){
+       println("Minimum speed is: $minSpeed and maximum speed is: $maxSpeed")
+    }
     fun drive(){
         println("Driving...")
     }
