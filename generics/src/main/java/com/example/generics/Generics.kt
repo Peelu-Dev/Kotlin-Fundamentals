f4e24c379ec95474e734abeff16dbd1c0126ab99
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-10 12:59:20 +0530

    Added Generics Type to avoid duplication of code

--- generics/src/main/java/com/example/generics/Generics.kt
+++ generics/src/main/java/com/example/generics/Generics.kt
@@ -1,19 +1,23 @@
 package com.example.generics
 
 fun main(){
-    val listOfNames = listOf("Peelu","Harshu","Sehwag","Sachin")
-    val finder = Finder(listOfNames)
-    finder.findItem("new"){
+//    val listOfNames = listOf("Peelu","Harshu","Sehwag","Sachin")
+    val listOfNumbers = listOf(12,23,46)
+    val finder = Finder(listOfNumbers)
+    finder.findItem(65){
         println("Found $it")
     }
 }
 
-class Finder(private val list: List<String>){
-    fun findItem(element:String,foundItem:(element:String?)-> Unit){
+class Finder<T>(private val list: List<T>){
+    fun findItem(element:T,foundItem:(element:T?)-> Unit){
         val itemFoundList = list.filter {
             it == element
         }
-        if(itemFoundList.isEmpty())
-            foundItem("Null") else foundItem(itemFoundList.first())
+        if(itemFoundList.isEmpty()) {
+            foundItem(null)
+        } else {
+            foundItem(itemFoundList.first())
+        }
     }
 }
